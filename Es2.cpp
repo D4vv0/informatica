a6f9206344e0f879fa55feffa18c94d11ed5a706
D4vv0 <47255065+D4vv0@users.noreply.github.com>
2019-02-01 19:32:15 +0100

    Add files via upload

---
+++ Es2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void stampa_base(char c);
+int main()
+{
+	char scelta;
+	cout << "Inserisci una lettera A,C,T,G: ";
+	cin >> scelta;
+	stampa_base(scelta);
+	return 0;
+}
+void stampa_base(char c)
+{
+	switch(c)
+	{
+		case 'A':
+			cout << "\nAdenina\n";
+		break;
+
+		case 'C':
+			cout << "\nCitosina\n";
+		break;
+
+		case 'T':
+			cout << "\nTimina\n";
+		break;
+
+		case 'G':
+			cout << "\nGuanina\n";
+		break;
+
+		default:
+			cout << "\n*ERRORE*\n";
+		break;
+	}
+}
